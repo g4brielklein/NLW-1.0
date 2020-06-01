94e5bfa70f54e18f55e396aeb4a6efd000e0d380
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-06-01 15:23:47 -0300

    Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários');
+    console.log('Listagem de usuários');
 
     response.json([ 
         'Diego',
